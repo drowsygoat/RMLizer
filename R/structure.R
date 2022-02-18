96103f670b9dc7c37bc57f47e2eace851f8edc57
Lechu <32292322+drowsygoat@users.noreply.github.com>
2022-02-18 03:21:49 +0100

    test

---
+++ R/structure.R
@@ -0,0 +1,9 @@
+# get_proportions()
+# --> obtain approximation of L and U for each transcript in the sample
+# get_noise_leves_from_spikes()
+# --> estimates noise levels from ERCC (default) or other spike in controls
+# "^ENSMUST.*$" --> transcripts
+# "^ENSMUSG.*$" --> genes
+# "^ERCC*$" --> ERCCs
+#
+## bedtools works with intersen=cting with exons, the ones that s
